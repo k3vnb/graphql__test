7f4474468bcb170b19ea2c68ba06bc8f2597ba0d
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-08 12:25:41 -0700

    create query schema from dummy data set

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const books = require('./store');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { books, authors } = require("./store");
 const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLBoolean,
-} = require('graphql');
+  GraphQLNonNull,
+  GraphQLList,
+} = require("graphql");
 
 const app = express();
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'HelloWorld',
-    fields: () => ({
-      message: { type: GraphQLString, resolve: () => 'Hello Worlds' },
-      numero: { type: GraphQLInt, resolve: () => 12 },
-      bool: { type: GraphQLBoolean, resolve: () => true },
-    })
-  })
+console.log(books, authors)
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+  }),
 });
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
-app.listen(5000, () => console.log('Server running on Port 5000'));
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of All Books",
+      resolve: () => books,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+app.listen(5000, () => console.log("Server running on Port 5000"));
--- store.js
+++ store.js
@@ -1,44 +1,60 @@
-const books = [
-  {
-    id: 1,
-    name: 'Harry Potter and the Chamber of Secrets',
-    authorId: 1,
-  },
-  {
-    id: 2,
-    name: 'Harry Potter and the Prisoner of Azkaben',
-    authorId: 1,
-  },
-  {
-    id: 3,
-    name: 'Harry Potter and the Oktoberfest Pretzel',
-    authorId: 1,
-  },
-  {
-    id: 4,
-    name: 'The Fellowship of the Rings',
-    authorId: 2,
-  },
-  {
-    id: 5,
-    name: 'The Two Towers',
-    authorId: 2,
-  },
-  {
-    id: 6,
-    name: 'The Return of the King',
-    authorId: 2,
-  },
-  {
-    id: 7,
-    name: 'The Way of the Shadows',
-    authorId: 3,
-  },
-  {
-    id: 8,
-    name: 'Beyond the Shadows',
-    authorId: 3,
-  },
-]
+const store = {
+  books: [
+    {
+      id: 1,
+      name: "Harry Potter and the Chamber of Secrets",
+      authorId: 1,
+    },
+    {
+      id: 2,
+      name: "Harry Potter and the Prisoner of Azkaben",
+      authorId: 1,
+    },
+    {
+      id: 3,
+      name: "Harry Potter and the Oktoberfest Pretzel",
+      authorId: 1,
+    },
+    {
+      id: 4,
+      name: "The Fellowship of the Rings",
+      authorId: 2,
+    },
+    {
+      id: 5,
+      name: "The Two Towers",
+      authorId: 2,
+    },
+    {
+      id: 6,
+      name: "The Return of the King",
+      authorId: 2,
+    },
+    {
+      id: 7,
+      name: "The Way of the Shadows",
+      authorId: 3,
+    },
+    {
+      id: 8,
+      name: "Beyond the Shadows",
+      authorId: 3,
+    },
+  ],
+  authors: [
+    {
+      id: 1,
+      name: "JK Rowling",
+    },
+    {
+      id: 2,
+      name: "JRR Tolkien",
+    },
+    {
+      id: 3,
+      name: "BrentWeeks",
+    },
+  ],
+};
 
-module.exports = books;
+module.exports = store;
