2c2e1b1cb62b2b8b3c1e4a7ec680c2306710e854
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-08 12:04:05 -0700

    build basic graphql structure

---
+++ store.js
@@ -0,0 +1,44 @@
+const books = [
+  {
+    id: 1,
+    name: 'Harry Potter and the Chamber of Secrets',
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: 'Harry Potter and the Prisoner of Azkaben',
+    authorId: 1,
+  },
+  {
+    id: 3,
+    name: 'Harry Potter and the Oktoberfest Pretzel',
+    authorId: 1,
+  },
+  {
+    id: 4,
+    name: 'The Fellowship of the Rings',
+    authorId: 2,
+  },
+  {
+    id: 5,
+    name: 'The Two Towers',
+    authorId: 2,
+  },
+  {
+    id: 6,
+    name: 'The Return of the King',
+    authorId: 2,
+  },
+  {
+    id: 7,
+    name: 'The Way of the Shadows',
+    authorId: 3,
+  },
+  {
+    id: 8,
+    name: 'Beyond the Shadows',
+    authorId: 3,
+  },
+]
+
+module.exports = books;
