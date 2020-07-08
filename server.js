ff94199fab8d9026ca6041f7d803e5154d184bc8
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-07-08 15:00:21 -0700

    add queryable objects for searching individual books & authors

--- server.js
+++ server.js
@@ -43,6 +43,14 @@ const RootQueryType = new GraphQLObjectType({
   name: "Query",
   description: "Root Query",
   fields: () => ({
+    book: {
+      type: BookType,
+      description: 'A Single Book',
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (_parent, args) => books.find(({ id }) => id === args.id),
+    },
     books: {
       type: new GraphQLList(BookType),
       description: "List of All Books",
@@ -52,6 +60,14 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(AuthorType),
       description: "List of All Authors",
       resolve: () => authors,
+    },
+    author: {
+      type: AuthorType,
+      description: "A Single Author",
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (_parent, args) => authors.find(({ id }) => id === args.id)
     },
   }),
 });
